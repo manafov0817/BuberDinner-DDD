7376ecd4f26ddd7c65c3a3fca704ee1292e3d88b
Mahammad Manafov <manafov0817@gmail.com>
2024-10-10 22:39:26 +0400

    JWT authentication added

---
+++ BuberDinner.Api/Controllers/DinnerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers
+{
+    [Route("[controller]")]
+    [Authorize]
+    public class DinnersController : ApiController
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(Array.Empty<string>());
+        }
+    }
+}
--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -13,9 +13,9 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 {
     app.UseExceptionHandler("/error");
-
     app.UseHttpsRedirection();
-
+    app.UseAuthentication();
+    app.UseAuthorization();
     app.MapControllers();
 
     app.Run();
